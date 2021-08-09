0488fef273159a958852dc882715b2af6bbe68db
Evon Perez <perez.evon@gmail.com>
2021-08-09 10:18:38 -0400

    working code

--- src/PokeItem.js
+++ src/PokeItem.js
@@ -4,11 +4,12 @@ class PokeItem extends Component {
     
     render() { 
         //whole object
-        const{url, pokemon, _id} = this.props;
+        const{url, pokemon, _id, ability} = this.props;
         return ( 
 
             <p>
-                <img src={url} id={_id} alt={pokemon}  width='200px'/>
+                <img src={url} id={_id} alt={pokemon} width='200px'/>
+                <label ability={ability}> Pokemon: {pokemon}---Ability: {ability}</label>
             </p>
 
          );
